9bafafa8594be7ee79058abee0bba76c058bc8b1
Shao Zengwei <zengwei19861029@163.com>
2015-09-01 22:03:19 +0800

    fix route configuration

---
+++ src/haxe/com/thoughtworks/restRpc/core/RouteConfiguration.hx
@@ -0,0 +1,25 @@
+package com.thoughtworks.restRpc.core;
+
+import scala.collection.Iterator;
+import com.qifun.jsonStream.JsonStream;
+
+interface IRouteConfiguration {
+    public function nameToUriTemplate(name: String) : IUriTemplate;
+}
+
+/**
+ *
+ **/
+@:nativeGen
+interface IUriTemplate {
+
+    public var method(get, never): String;
+
+    private function get_method():String;
+
+    /**
+     *约定参数列表的无法被uri template消费的参数（应该是最后一个）作为请求体
+     **/
+    public function render(parameters: Iterator<JsonStream>):String;
+
+}
